692d21fd5c75b551238612a8349235c765c088c5
Dmytro Hadiuchko <hadiuchko.dmytro@gmail.com>
2026-02-08 14:13:28 +0200

    Add User entity with JPA annotations

---
+++ src/main/java/ua/tms/budgetmanager/model/User.java
@@ -0,0 +1,18 @@
+package ua.tms.budgetmanager.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+    @Id
+    private Long id;
+
+    @Column
+    private String username;
+}
