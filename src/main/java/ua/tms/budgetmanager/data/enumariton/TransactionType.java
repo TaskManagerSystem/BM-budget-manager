5bdb401902c90ae9690b6d1d9dfdffb775f0309f
Oleksandr Farion <oleksandr.farion12@gmail.com>
2026-02-11 12:12:30 +0200

    chore: [BM-11] Configured liquibase and postgres (#4)
    * chore: [BM-11] added liquibase starter
    
    * chore: [BM-11] configured postgres and liquibase
    
    * chore: [BM-11] added config for tests
    
    * feature: [BM-13] Implemented transaction layer (#5)
    
    * feat: [BM-13] added transaction entity
    
    * feat: [BM-13] added init script for transactions
    
    * feat: [BM-13] implemented repository
    
    * feat: [BM-13] removed redundant category
    
    * feat: [BM-13] modified fields
    
    * feat: [BM-13] modified fields
    
    * feature: [BM-14] Implemented wallet layer (#6)
    
    * feat: [BM-14] implemented wallet entity and modified init script
    
    * feat: [BM-14] implemented repository
    
    ---------
    
    Co-authored-by: reamfox <oleksandr.farion12@gmailcom>
    
    ---------
    
    Co-authored-by: reamfox <oleksandr.farion12@gmailcom>

---
+++ src/main/java/ua/tms/budgetmanager/data/enumariton/TransactionType.java
@@ -0,0 +1,8 @@
+package ua.tms.budgetmanager.data.enumariton;
+
+import java.io.Serializable;
+
+public enum TransactionType implements Serializable {
+  INCOME,
+  EXPENSE
+}
---
+++ src/main/java/ua/tms/budgetmanager/data/enumariton/WalletType.java
@@ -0,0 +1,8 @@
+package ua.tms.budgetmanager.data.enumariton;
+
+import java.io.Serializable;
+
+public enum WalletType implements Serializable {
+  CASH,
+  CARD
+}
